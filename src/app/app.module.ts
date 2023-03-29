c63c10f06550d3d8e8a117bdeb36b5c68510b5b4
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-03-29 13:40:12 +0200

    nav och footer styling done, not title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    RecipeCardComponent
   ],
   imports: [
     BrowserModule,
