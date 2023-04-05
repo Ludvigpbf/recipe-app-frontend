2c0d3d50960e5ff498f494bb1d38770cd05c6d4b
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-04-05 15:17:31 +0200

    login + get user funkar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth-service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  //from form
+  me = {
+    id: 0,
+    name: '',
+    email: 'nkoch@example.org',
+    password: 'password',
+  };
 
+  user1 = {
+    id: 0,
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService) {
+    //authService.loginUser(this.me); // ska ej ha denna!!!
+  }
+
+  login() {
+    this.authService.loginUser(this.me);
+  }
+
+  getUser() {
+    this.authService.getUser1().subscribe((res) => {
+      console.log(res[0]);
+      this.user1 = res[0];
+    });
+  }
 }
--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,9 +1,73 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { User } from 'src/app/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  configUrl = 'https://recipe-app-backend-production.up.railway.app/api/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ',
+    }),
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /* registerUser(user: User){
+
+  } */
+
+  loginUser(user: User) {
+    this.http
+      .post<any>(this.configUrl + 'login', user, this.httpOptions)
+      .pipe(catchError(this.handleError))
+      .subscribe((res) => {
+        console.log(res);
+        localStorage.setItem('token', res.token);
+      });
+  }
+
+  /* logoutUser(user: User){
+    localStorage.removeItem('token', res.token);
+  } */
+
+  getUser1() {
+    console.log(localStorage.getItem('token'));
+    this.httpOptions.headers = this.httpOptions.headers.set(
+      'Authorization',
+      'Bearer ' + localStorage.getItem('token')
+    );
+    return this.http
+      .get<User[]>(this.configUrl + 'getuser/1', this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
 }
