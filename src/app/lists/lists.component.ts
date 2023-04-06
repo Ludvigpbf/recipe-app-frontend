6ee98ecf347de8c8cbffe68189ac9f1594a9d6b4
Ludvigpbf <ludvig.flyckt@gmail.com>
2023-04-06 23:54:19 +0200

    fixat lite styling i searchrecipe

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../services/recipe-service/recipe.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.scss']
+  styleUrls: ['./lists.component.scss'],
 })
 export class ListsComponent {
+  favRecipeShow = false;
+  loadRecipes = false;
+  oneRecipe: any;
+  id = '';
+  ngOnInit() {
+    this.loadRecipes = true;
+    // should be function: getFavouriteRecipes()
+    this.recipeService.getRecipeId(this.id).subscribe((result) => {
+      this.favRecipeShow = true;
+      this.loadRecipes = false;
+      this.oneRecipe = result;
+      /* console.log(recipes); */ // Prints recipes
+    });
+  }
 
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 }
